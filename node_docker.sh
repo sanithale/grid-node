b7b8cc64cceef23f07860cd45e38807bc52304ad
Sanit Thale <sanit.thale@morningstar.com>
2018-03-21 13:12:47 +0530

    Modified

--- node_docker.sh
+++ node_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
 echo "Running Grid Start Node"
 # Get Running Instance Local IP
 export EC2_INSTANCE_IP="`wget -q -O - http://169.254.169.254/latest/meta-data/local-ipv4 || die \"wget instance-id has failed: $?\"`"
 # Run Docker
 docker run -d -p 5555:5555 -e REMOTE_HOST="http://$EC2_INSTANCE_IP:5555" -e HUB_PORT_4444_TCP_ADDR=10.255.21.225 -e HUB_PORT_4444_TCP_PORT=4444 selenium/node-chrome
+echo "Started Grid Node"
